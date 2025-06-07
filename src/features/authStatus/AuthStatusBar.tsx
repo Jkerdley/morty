b75d5d635c6df2f7fc34c222a2e5e39c4715f8ae
Jkerdley <jk-erley@yandex.ru>
2025-06-08 00:49:48 +0700

    feat: adds antdesign ui for user status bar adn to login form

--- src/features/authStatus/AuthStatusBar.tsx
+++ src/features/authStatus/AuthStatusBar.tsx
@@ -1,6 +1,8 @@
 import { useNavigate } from "react-router-dom";
 import styles from "./authStatus.module.css";
 import { useAuth } from "../../shared/hooks/useAuth";
+import { Avatar, Button } from "antd";
+import { UserOutlined, PoweroffOutlined } from "@ant-design/icons";
 
 export const AuthStatusBar = () => {
   const navigate = useNavigate();
@@ -12,8 +14,21 @@ export const AuthStatusBar = () => {
     return <div className={styles.auth}>You are not logged in</div>;
   return (
     <div className={styles.auth}>
-      <p>Welcome {auth?.user.username}</p>
-      <button onClick={handleLogout}>Выйти из аккаунта</button>
+      <div className={styles.authPerson}>
+        <p>Welcome {auth?.user.username}</p>
+        <Avatar
+          style={{ backgroundColor: "#87d068" }}
+          icon={<UserOutlined />}
+        />
+      </div>
+      <Button
+        className={styles.customGreenBtn}
+        type="primary"
+        icon={<PoweroffOutlined />}
+        onClick={handleLogout}
+      >
+        Выйти из аккаунта
+      </Button>
     </div>
   );
 };
--- src/features/loginForm/LoginForm.tsx
+++ src/features/loginForm/LoginForm.tsx
@@ -1,37 +1,107 @@
-import type { FormEvent } from "react";
 import styles from "./loginForm.module.css";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useAuth } from "../../shared/hooks";
+import { Button, Checkbox, Form, Input, message } from "antd";
+import type { FormProps } from "antd";
+
+type FieldType = {
+  username?: string;
+  password?: string;
+  remember?: boolean;
+};
 
 export const LoginForm = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const auth = useAuth();
-
+  const [form] = Form.useForm();
   const from = location.state?.from || "/";
 
-  const handleSubmitForm = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget);
-    const username = formData.get("username")?.toString();
-    const password = formData.get("password")?.toString();
+  const onFinish: FormProps<FieldType>["onFinish"] = (values) => {
+    const { username, password } = values;
 
-    if (!username || !password) {
-      alert('Заполните поля формы "username" и "password"');
+    if (username && password) {
+      auth?.signIn({ username, password }, () => {
+        navigate(from, { replace: true });
+      });
     } else {
-      auth?.signIn({ username, password }, () =>
-        navigate(from, { replace: true })
-      );
+      message.error('Заполните поля формы "username" и "password"');
     }
   };
+
+  const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = (
+    errorInfo
+  ) => {
+    console.log("Validation failed:", errorInfo);
+    message.error("Пожалуйста, заполните все обязательные поля");
+  };
+
   return (
     <div className={styles.loginPageContainer}>
-      <form onSubmit={handleSubmitForm} id="loginForm" name="loginForm">
-        <label htmlFor="loginForm">Форма входа</label>
-        Username: <input type="text" name="username" />
-        Password: <input type="password" name="password" />
-        <button type="submit">Login</button>
-      </form>
+      <Form
+        form={form}
+        name="login"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 600 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item<FieldType>
+          className={styles.formLabel}
+          label={<span className={styles.formLabel}>Имя</span>}
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: (
+                <span className={styles.validationMessage}>
+                  Пожалуйста, введите имя пользователя!
+                </span>
+              ),
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item<FieldType>
+          label={<span className={styles.formLabel}>Пароль</span>}
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: (
+                <span className={styles.validationMessage}>
+                  Пожалуйста, введите пароль!
+                </span>
+              ),
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item<FieldType>
+          name="remember"
+          valuePropName="checked"
+          wrapperCol={{ offset: 8, span: 16 }}
+        >
+          <Checkbox className={styles.checkboxLabel}>Запомнить меня</Checkbox>
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button
+            className={styles.customGreenBtn}
+            type="primary"
+            htmlType="submit"
+          >
+            Войти в портал Рика
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 };
